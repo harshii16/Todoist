89804a691930bf3d3af0e4de26869681ae5d3699
harshii1611 <harshita0gupta#@gmail.com>
2024-05-29 12:46:15 +0530

    Initial commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <nav className = 'flex justify-between bg-indigo-900 text-white py-4'>
+      <div className="logo">
+        <span className='font-bold text-xl mx-8'>Todoist</span>
+      </div>
+      <ul className="flex gap-8">
+        <li className='cursor-pointer hover:font-bold transition-all'>Home</li>
+        <li className='cursor-pointer hover:font-bold transition-all'>Your Taks</li>
+
+      </ul>
+
+    </nav>
+  )
+}
+
+export default Navbar
